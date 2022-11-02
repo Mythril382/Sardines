2e50aa7fa4d70306ceec4e565490dbac45366acb
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-02 20:02:46 +0800

    more vars

--- src/sardines/leveling/Levelable.java
+++ src/sardines/leveling/Levelable.java
@@ -4,18 +4,25 @@ import arc.func.*;
 import arc.struct.*;
 import mindustry.ctype.*;
 
+import static arc.Core.*;
+
 public class Levelable{
     public UnlockableContent content;
     public Seq<Cons<UnlockableContent>> levelCons;
+    protected int level = 0, maxLevel;
     
-    public Levelable(UnlockableContent content, Seq<Cons<UnlockableContent>> levelCons){
+    public Levelable(UnlockableContent content, Seq<Cons<UnlockableContent>> levelCons, int maxLevel){
         this.content = content;
         this.levelCons = levelCons;
+        level = settings.getInt(content.name + "-sardlevel", 0);
+        this.maxLevel = maxLevel;
     }
     
     public void runCons(int level){
         Cons<UnlockableContent> cons = levelCons.get(level);
-        if(cons == null) return;
+        if(cons == null || level == maxLevel) return;
         cons.get(content);
+        this.level = level;
+        settings.put(content.name + "-sardlevel", level);
     }
 }
