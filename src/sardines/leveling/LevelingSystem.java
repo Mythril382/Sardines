d9d7a878bc5cb1fd086558b50a111a73f09b887a
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-06 05:35:42 +0800

    lots of stuff

--- src/sardines/leveling/LevelingSystem.java
+++ src/sardines/leveling/LevelingSystem.java
@@ -18,22 +18,34 @@ public class LevelingSystem{
                 Turret b = (Turret)c;
                 b.health = 250;
                 b.requirements = ItemStack.with(Items.copper, 35);
+                b.reload = 20f;
                 b.shoot.shots = 1;
                 b.shoot.shotDelay = 0f;
+                b.ammoTypes.get(Items.copper).damage = 9f;
+                b.ammoTypes.get(Items.graphite).damage = 18f;
+                b.ammoTypes.get(Items.silicon).damage = 12f;
             },
             c -> {
                 Turret b = (Turret)c;
                 b.health = 360;
                 b.requirements = ItemStack.with(Items.copper, 50);
-                b.shoot.shots = 2;
+                b.reload = 18f;
+                b.shoot.shots = 3;
                 b.shoot.shotDelay = 5f;
+                b.ammoTypes.get(Items.copper).damage = 11f;
+                b.ammoTypes.get(Items.graphite).damage = 20f;
+                b.ammoTypes.get(Items.silicon).damage = 14f;
             },
             c -> {
                 Turret b = (Turret)c;
                 b.health = 520;
                 b.requirements = ItemStack.with(Items.copper, 85, Items.lead, 30);
-                b.shoot.shots = 4;
+                b.reload = 16f;
+                b.shoot.shots = 5;
                 b.shoot.shotDelay = 2.5f;
+                b.ammoTypes.get(Items.copper).damage = 14f;
+                b.ammoTypes.get(Items.graphite).damage = 23f;
+                b.ammoTypes.get(Items.silicon).damage = 15f;
             }
         ));
         level(Blocks.duo, settings.getInt("duo-slevel", 0));
