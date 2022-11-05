c2aad9f396094c991313e8b74333f22e0101605f
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-05 16:54:47 +0800

    mmmm

---
+++ src/sardines/core/SardUI.java
@@ -0,0 +1,11 @@
+package sardines.core;
+
+import sardines.ui.*;
+
+public class SardUI{
+    public LevelingDialog level;
+    
+    public SardUI(){
+        level = new LevelingDialog();
+    }
+}
