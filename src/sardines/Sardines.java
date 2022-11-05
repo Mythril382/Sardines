5e460e3ae73cbca68f40f72a1dcaae510942a702
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-05 17:08:20 +0800

    epic

--- src/sardines/Sardines.java
+++ src/sardines/Sardines.java
@@ -1,5 +1,22 @@
 package sardines;
 
+import arc.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
 import mindustry.mod.*;
+import sardines.leveling.*;
 
-public class Sardines extends Mod{}
+import static mindustry.Vars.*;
+
+public class Sardines extends Mod{
+    public Sardines(){
+        Events.on(ClientLoadEvent.class, e -> {
+            Vars.ui.menufrag.addButton("@sardui.level", Icon.effect, SardVars.ui.level::show);
+        });
+    }
+    
+    @Override
+    public void loadContent(){
+        LevelingSystem.load();
+    }
+}
