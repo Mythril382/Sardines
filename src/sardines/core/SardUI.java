3c1aee59661dfeb9841f6f1e9dc6acf7d77d1fd3
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-06 23:34:21 +0800

    i hate it

--- src/sardines/core/SardUI.java
+++ src/sardines/core/SardUI.java
@@ -3,11 +3,11 @@ package sardines.core;
 import sardines.ui.*;
 
 public class SardUI{
-    public LevelingDialog level;
     public ProductionDialog prod;
+    public LevelingDialog level;
     
     public SardUI(){
-        level = new LevelingDialog();
         prod = new ProductionDialog();
+        level = new LevelingDialog();
     }
 }
