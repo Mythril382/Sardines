456d997e99cb5e804827470d05859306dc34f099
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-06 20:18:30 +0800

    carb

--- src/sardines/Sardines.java
+++ src/sardines/Sardines.java
@@ -1,17 +1,28 @@
 package sardines;
 
 import arc.*;
+import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 import sardines.leveling.*;
 
+import static arc.Core.*;
 import static mindustry.Vars.*;
 
 public class Sardines extends Mod{
+    public float counter = 0f;
+    
     public Sardines(){
         Events.on(ClientLoadEvent.class, e -> {
             ui.menufrag.addButton("@leveling", Icon.effect, SardVars.ui.level::show);
+            Events.run(Trigger.update, () -> {
+                counter += Time.delta;
+                if(counter >= (60 * 60)){
+                    settings.put("mini-carbs", settings.getInt("mini-carb-prods", 5) * 1);
+                    counter = 0f;
+                }
+            });
         });
     }
     
