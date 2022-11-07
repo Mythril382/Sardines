bfd19711d720c4b1e8c48d3e661c0019c084452c
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-08 05:29:55 +0800

    real

---
+++ src/sardines/ui/LStatementInfoDialog.java
@@ -0,0 +1,54 @@
+package sardines.ui;
+
+import arc.func.*;
+import arc.scene.ui.layout.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
+import mindustry.ui.dialogs.*;
+
+import static arc.Core.*;
+import static mindustry.Vars.*;
+
+public class LStatementInfoDialog extends BaseDialog{
+    protected final ObjectMap<String, Func<LStatement, String>> lStats = ObjectMap.of(
+        "privileged", lst -> bundle.get(lst.privileged() ? "yes" : "no"),
+        "nonprivileged", lst -> bundle.get(lst.nonPrivileged() ? "yes" : "no"),
+        "category", lst -> lst.category().localized()
+    );
+    
+    public LStatementInfoDialog(){
+        super("@info.title");
+        addCloseButton();
+    }
+    
+    public void show(LStatement statement){
+        String smallName = statement.name.toLowerCase().replace(" ", "")
+        cont.clear();
+        Table table = new Table();
+        table.margin(10);
+        table.table(title1 -> {
+            title1.image(Icon.logic).size(iconXLarge).scaling(Scaling.fit);
+            title1.add("[accent]" + statement.name() + (settings.getBool("console") ? "\n[gray]" + smallName : "")).padLeft(10);
+        });
+        table.row();
+        if(bundle.has(smallName)){
+            table.add("@category.purpose").color(Pal.accent).fillX().padTop(10);
+            table.row();
+            table.add("[lightgray]" + bundle.get(smallName)).wrap().fillX().padLeft(10).width(500f).left();
+            table.row();
+        }
+        lStats.each((k, v) -> {
+            table.table(inset -> {
+                inset.left();
+                inset.add("[lightgray]" + bundle.get("lstat." + v) + ":[] " + v.get(statement)).left().top();
+            });
+            table.row();
+        });
+        ScrollPane pane = new ScrollPane(table);
+        cont.add(pane);
+        show();
+    }
+}
