93538fcba761e8b4dceadb41da687bc97ea5befb
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-08 06:39:31 +0800

    real

--- src/sardines/core/SardUI.java
+++ src/sardines/core/SardUI.java
@@ -5,9 +5,11 @@ import sardines.ui.*;
 public class SardUI{
     public ProductionDialog prod;
     public LevelingDialog level;
+    public LStatementInfoDialog lstInfo;
     
     public SardUI(){
         prod = new ProductionDialog();
         level = new LevelingDialog();
+        lstInfo = new LStatementInfoDialog();
     }
 }
