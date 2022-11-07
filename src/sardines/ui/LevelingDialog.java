5853e32cfac738eca1a41d63f46e701af4157391
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-08 06:10:38 +0800

    more spaces

--- src/sardines/ui/LevelingDialog.java
+++ src/sardines/ui/LevelingDialog.java
@@ -17,11 +17,15 @@ import static mindustry.Vars.*;
 public class LevelingDialog extends BaseDialog{
     public LevelingDialog(){
         super("@leveling");
+        
         addCloseButton();
+        
         buttons.button("@mini-carb.extra", Icon.upload, () -> ui.showInfo("[lightgray]..."));
         buttons.button("@mini-carb.prods", Icon.hammer, () -> { if(SardVars.ui.prod != null) SardVars.ui.prod.show(); });
+        
         shown(() -> {
             this.rebuild();
+            
             boolean hintDone = settings.getBool("leveling-hint", false);
             if(!hintDone) ui.showInfo("@leveling.hint");
             settings.put("leveling-hint", true);
@@ -31,33 +35,41 @@ public class LevelingDialog extends BaseDialog{
     
     void rebuild(){
         cont.clear();
+        
         Seq<Levelable> all = LevelingSystem.all();
+        
         cont.table(t -> {
             t.setBackground(Tex.whiteui);
             t.setColor(Pal.darkishGray);
             t.image(Items.carbide.uiIcon).size(iconXLarge).scaling(Scaling.fit).pad(5);
             t.label(() -> Integer.toString(settings.getInt("mini-carbs", 0))).style(Styles.outlineLabel).pad(5);
         }).pad(20).row();
+        
         cont.table(t -> {
             t.setBackground(Tex.whiteui);
             t.setColor(Pal.darkishGray);
             t.image(Blocks.carbideCrucible.uiIcon).size(iconXLarge).scaling(Scaling.fit).pad(5);
             t.label(() -> Integer.toString(settings.getInt("mini-carb-prods", 5))).style(Styles.outlineLabel).pad(5);
         }).pad(20).row();
+        
         cont.pane(list -> {
             all.each(l -> {
                 UnlockableContent c = l.content;
+                
                 list.table(t -> {
                     t.setBackground(Tex.button);
+                    
                     t.table(t1 -> {
                         t1.image(c.uiIcon).size(iconXLarge).scaling(Scaling.fit);
                         t1.add("[accent]" + c.localizedName).padLeft(5);
                     });
                     t.row();
+                    
                     if(c.description != null){
                         t.add("[lightgray]" + c.displayDescription()).wrap().fillX().padLeft(0).width(500f).padTop(10).left();
                         t.row();
                     }
+                    
                     t.button(l.level() < l.levelCons.size - 1 ? bundle.format("leveling.currentnext", l.level() + 1, l.level() + 2, l.price(l.level() + 1)) : "@leveling.max", () -> {
                         if(l.level() >= l.levelCons.size - 1){
                             ui.showInfo("@leveling.maxalready");
