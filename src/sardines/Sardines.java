20fe0ba4a037d1552266de8a63a1faa92faa28cb
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-08 06:41:03 +0800

    hopefully this works

--- src/sardines/Sardines.java
+++ src/sardines/Sardines.java
@@ -1,9 +1,16 @@
 package sardines;
 
 import arc.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.scene.ui.*;
+import arc.scene.ui.layout.*;
+import arc.struct.*;
 import arc.util.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.logic.*;
 import mindustry.mod.*;
 import sardines.leveling.*;
 
@@ -16,12 +23,19 @@ public class Sardines extends Mod{
     public Sardines(){
         Events.on(ClientLoadEvent.class, e -> {
             ui.menufrag.addButton("@leveling", Icon.effect, SardVars.ui.level::show);
+            
             Events.run(Trigger.update, () -> {
                 counter += Time.delta;
+                
                 if(counter >= (60 * 60)){
                     settings.put("mini-carbs", settings.getInt("mini-carbs") + settings.getInt("mini-carb-prods", 5) * 1);
                     counter = 0f;
                 }
+            });
+            
+            ui.database.shown(this::setupDatabase);
+            Events.run(ResizeEvent.class, () -> {
+                setupDatabase();
             });
         });
     }
@@ -29,5 +43,35 @@ public class Sardines extends Mod{
     @Override
     public void loadContent(){
         LevelingSystem.load();
+    }
+    
+    public void setupDatabase(){
+        Table db = (Table)Reflect.get(ui.database, "all");
+        TextField search = (TextField)Reflect.get(ui.database, "search");
+        Seq<LStatement> statements = LogicIO.allStatements.copy().map(prov -> prov.get()).select(lst -> lst.category() != LCategory.unknown && (search.getText().isEmpty() || lst.name().toLowerCase().contains(search.getText().toLowerCase())));
+        
+        db.add("@content.lstatement.name").growX().left().color(Pal.spore);
+        db.row();
+        db.image().growX().pad(5).padLeft(0).padRight(0).height(3).color(Pal.spore);
+        db.row();
+        
+        db.table(list -> {
+            list.left();
+            
+            int cols = (int)Mathf.clamp((graphics.getWidth() - Scl.scl(30)) / Scl.scl(32 + 12), 1, 22);
+            int count = 0;
+            
+            for(int i = 0; i < statements.size; i++){
+                LStatement lst = statements.get(i);
+                Image image = list.add(Icon.logic).color(lst.category().color).size(8 * 4).pad(3).get();
+                
+                image.clicked(() -> SardVars.ui.lstInfo.show(lst));
+                
+                if((++count) % cols == 0){
+                    list.row();
+                }
+            }
+        }).growX().left().padBottom(20);
+        db.row();
     }
 }
