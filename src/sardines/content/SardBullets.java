898b10136895daea7aa6e3c9c4ae74f17ff156b5
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-21 22:08:19 +0800

    a

--- src/sardines/content/SardBullets.java
+++ src/sardines/content/SardBullets.java
@@ -9,6 +9,8 @@ public class SardBullets{
     public static void load(){
         wallBowling = new BowlingBulletType(1f, 150f){{
             width = height = 4f * 32f;
+            lifetime = 60f * 2.5f;
+            spin = 3f;
         }};
     }
 }
