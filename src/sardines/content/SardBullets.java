266103a9d06f32e79b220ebc30e92ba1359fb9b7
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-21 22:29:39 +0800

    bruh

--- src/sardines/content/SardBullets.java
+++ src/sardines/content/SardBullets.java
@@ -10,9 +10,9 @@ public class SardBullets{
     public static void load(){
         wallBowling = new BowlingBulletType(1f, 150f){{
             width = height = 6f;
+            shrinkX = shrinkY = 0f;
             lifetime = 60f * 2.5f;
             spin = 3f;
-            
             layer = 99f;
             frontColor = trailColor = Color.valueOf("ffffff");
             trailLength = 20f;
