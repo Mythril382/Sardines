6ab725064b5a1c24d4e87963c493bbbfbb4d6265
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-24 23:17:04 +0800

    tita

--- src/sardines/content/SardBullets.java
+++ src/sardines/content/SardBullets.java
@@ -7,7 +7,10 @@ import sardines.entities.bullet.*;
 
 public class SardBullets{
     public static BulletType
-    copperWallBowling;
+    
+    // wall bowling
+    copperWallBowling,
+    titaniumWallBowling;
     
     public static void load(){
         copperWallBowling = new BowlingBulletType(3f, 200f){{
@@ -20,6 +23,19 @@ public class SardBullets{
             trailColor = Color.valueOf("d99d73");
             trailLength = 35;
             trailWidth = 3f;
+            hitSound = Sounds.bang;
+        }};
+        
+        titaniumWallBowling = new BowlingBulletType(3f, 400f){{
+            width = height = 8f;
+            pierceCap = 6;
+            shrinkX = shrinkY = 0f;
+            lifetime = 60f * 3f;
+            spin = 5f;
+            frontColor = Color.valueOf("ffffff");
+            trailColor = Color.valueOf("8da1e3");
+            trailLength = 45;
+            trailWidth = 4f;
             hitSound = Sounds.bang;
         }};
     }
