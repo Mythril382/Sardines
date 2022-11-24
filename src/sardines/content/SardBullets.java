aa1e00fc8d406ae14caf007f18225d3be75bec47
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-11-24 14:21:46 +0800

    yay(?)

--- src/sardines/content/SardBullets.java
+++ src/sardines/content/SardBullets.java
@@ -2,6 +2,7 @@ package sardines.content;
 
 import arc.graphics.*;
 import mindustry.entities.bullet.*;
+import mindustry.gen.*;
 import sardines.entities.bullet.*;
 
 public class SardBullets{
@@ -9,8 +10,9 @@ public class SardBullets{
     copperWallBowling;
     
     public static void load(){
-        copperWallBowling = new BowlingBulletType(1f, 150f){{
+        copperWallBowling = new BowlingBulletType(1f, 200f){{
             width = height = 6f;
+            pierceCap = 4;
             shrinkX = shrinkY = 0f;
             lifetime = 60f * 2.5f;
             spin = 3f;
@@ -19,6 +21,7 @@ public class SardBullets{
             trailColor = Color.valueOf("d99d73");
             trailLength = 35;
             trailWidth = 3f;
+            hitSound = Sounds.bang;
         }};
     }
 }
