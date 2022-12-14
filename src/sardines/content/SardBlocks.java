28731228d67c81ca69538457ca2a47a20a1127d6
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-12-14 23:33:56 +0800

    amaze

---
+++ src/sardines/content/SardBlocks.java
@@ -0,0 +1,25 @@
+package sardines.content;
+
+import mindustry.content.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+import sardines.world.blocks.defense.*;
+
+import static mindustry.type.ItemStack.*;
+
+public class SardBlocks{
+    public Block copperWallBowling, titaniumWallBowling;
+    
+    public static void load(){
+        copperWallBowling = new BowlingBlock("copper-wall-bowling"){{
+            requirements(Category.turret, with(Items.copper, 6));
+            bullet = SardBullets.copperWallBuilding;
+        }};
+        
+        titaniumWallBowling = new BowlingBlock("titanium-wall-bowling"){{
+            requirements(Category.turret, with(Items.titanium, 6));
+            bullet = SardBullets.titaniumWallBuilding;
+        }};
+    }
+}
