e9cd191512a796314500f2e90b6b3b87aab71825
Mythril382 <77225817+Mythril382@users.noreply.github.com>
2022-12-14 23:15:41 +0800

    real

---
+++ src/sardines/world/blocks/defense/BowlingBlock.java
@@ -0,0 +1,40 @@
+package sardines.world.blocks.defense.*;
+
+import arc.math.*;
+import mindustry.content.*;
+import mindustry.entities.bullet.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.world.*;
+
+import static mindustry.Vars.*;
+
+public class BowlingBlock extends Block{
+    public BulletType bullet;
+    
+    public BulletBlock(String name){
+        super(name);
+        
+        solid = false;
+        update = true;
+    }
+    
+    @Override
+    public boolean canPlaceOn(Tile tile, Team team, int rotation){
+        return mapBowling() ? tile.floor() == Blocks.sandWater : super.canPlaceOn(tile, team, rotation);
+    }
+    
+    public static boolean mapBowling(){
+        return state.map.description().contains("WALL-BOWL");
+    }
+    
+    public class BowlingBuild extends Building{
+        @Override
+        public void updateTile(){
+            float rotation = mapBowling() ? 0f : Mathf.random(0f, 360f);
+            bullet.create(this, team, x, y, rotation);
+            
+            tile.setAir();
+        }
+    }
+}
